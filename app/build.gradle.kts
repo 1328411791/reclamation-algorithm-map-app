4cb505b5f7d3478f370147e400b772447bde0d9e
liahnu <1328411791@qq.com>
2024-08-29 21:17:41 +0800

    update ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,9 @@ android {
     sourceSets {
         getByName("main") {
             assets {
-                srcDirs("src\\main\\assets", "src\\main\\assets")
+                srcDirs(
+                    "src\\main\\assets"
+                )
             }
         }
     }
